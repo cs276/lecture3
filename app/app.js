4bf84a8dd58a0de854b8e27c769dcceddcd9a15c
David J. Malan <malan@harvard.edu>
2018-09-18 08:44:26 -0400

    tidied

--- app/app.js
+++ app/app.js
@@ -1,63 +1,68 @@
-const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
+const express = require('express');
+const _ = require('lodash');
 const path = require('path');
-const app = express();
+const session = require('express-session');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.use(session({secret: 'keyboard cat'}));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded());
-
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
-
 const USERS = {'jharvard': 'crimson'};
 
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'keyboard cat'
+}));
+
 function loginRequired(req, res, next) {
-    if (!req.session.user_id) {
+    if (!req.session.username) {
         res.redirect('/login');
     } else {
         next();
     }
 }
 
-// if GET, show UL of registered usernames
 app.get('/', loginRequired, (req, res) => {
-    res.render('index', {users: Object.keys(USERS)});
+    res.render('index', {
+        username: req.session['username'],
+        users: _.keys(USERS)
+    });
 });
 
-// if GET, show form; if POST, look up user and password in USERS; if correct, set req.session.user_id and redirect to /
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
 app.post('/login', (req, res) => {
-    var username = req.body.username,
-        password = req.body.password;
-
-    if (!USERS[username] && USERS[username] != password) {
+    let {username, password} = req.body;
+    if (!USERS[username] || USERS[username] != password) {
         res.redirect('/login');
     } else {
-        req.session.user_id = 1;
+        req.session.username = username;
         res.redirect('/');
     }
 });
 
-// if GET, show form; if POST, add new user to USERS
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
 app.post('/register', (req, res) => {
-    var username = req.body.username,
-        password = req.body.password;
-        
+    let {username, password} = req.body;
     if (username && password) {
         USERS[username] = password;
     }
@@ -68,5 +73,3 @@ app.post('/register', (req, res) => {
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 });
-
-
