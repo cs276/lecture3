c294f0d7252cf4fde82a9d42cb24e1935f4b393c
David J. Malan <malan@harvard.edu>
2018-09-17 23:28:07 -0400

    adding app

--- app/app.js
+++ app/app.js
@@ -1,41 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const session = require('express-session')
+const path = require('path');
+const app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(session({secret: 'keyboard cat'}));
 
-var app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const USERS = {'jharvard': 'crimson'};
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+function loginRequired(req, res, next) {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+// if GET, show UL of registered usernames
+app.get('/', loginRequired, (req, res) => {
+    res.send('hello, world\n');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// if GET, show form; if POST, look up user and password in USERS; if correct, set req.session.user_id and redirect to /
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
-module.exports = app;
+// if GET, show form; if POST, add new user to USERS
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
+
+
