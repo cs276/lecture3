18e3e5c6107d89c86cc56d7718064eff77cdbb70
Colton Ogden <coltonoscopy@gmail.com>
2018-09-18 00:39:00 -0400

    login, register, view usernames

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const session = require('express-session')
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const hostname = '127.0.0.1';
@@ -11,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const USERS = {'jharvard': 'crimson'};
 
@@ -24,7 +26,7 @@ function loginRequired(req, res, next) {
 
 // if GET, show UL of registered usernames
 app.get('/', loginRequired, (req, res) => {
-    res.send('hello, world\n');
+    res.render('index', {users: Object.keys(USERS)});
 });
 
 // if GET, show form; if POST, look up user and password in USERS; if correct, set req.session.user_id and redirect to /
@@ -32,9 +34,32 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+app.post('/login', (req, res) => {
+    var username = req.body.username,
+        password = req.body.password;
+
+    if (!USERS[username] && USERS[username] != password) {
+        res.redirect('/login');
+    } else {
+        req.session.user_id = 1;
+        res.redirect('/');
+    }
+});
+
 // if GET, show form; if POST, add new user to USERS
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    var username = req.body.username,
+        password = req.body.password;
+        
+    if (username && password) {
+        USERS[username] = password;
+    }
+
+    res.redirect('/login');
 });
 
 app.listen(port, hostname, () => {
